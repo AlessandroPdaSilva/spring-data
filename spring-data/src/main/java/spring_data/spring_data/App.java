a541a2fec27beb9123012ed95bbd4a8e7476f18c
alessandro.silva <alessandro.silva@ESTACAO.SETUR.DF.GOV.BR>
2022-08-25 15:37:35 -0300

    Primeiro commit

---
+++ spring-data/src/main/java/spring_data/spring_data/App.java
@@ -0,0 +1,13 @@
+package spring_data.spring_data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ spring-data/src/test/java/spring_data/spring_data/AppTest.java
@@ -0,0 +1,20 @@
+package spring_data.spring_data;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
